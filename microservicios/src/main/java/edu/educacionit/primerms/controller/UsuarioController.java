0cb9cd78a9941802b6fe577c9606f47d32fda43c
EducaciónIT <EducacinIT@Alumno-1006-02>
2019-07-26 12:49:28 -0300

    asd

--- microservicios/src/main/java/edu/educacionit/primerms/controller/UsuarioController.java
+++ microservicios/src/main/java/edu/educacionit/primerms/controller/UsuarioController.java
@@ -7,24 +7,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.educacionit.primerms.model.Usuario;
+
 @RestController
 public class UsuarioController {
 
-	List<String> usuarios = new ArrayList<>();
-	{
-		usuarios.add("Franco");
-		usuarios.add("Pablo");
+	private static Integer id = 0; 
+
+	private static List<Usuario> usuarios;
+	static{
+		usuarios = new ArrayList<>();
+		usuarios.add(new Usuario(id++, "Franco"));
+		usuarios.add(new Usuario(id++, "Pablo"));
 	}
 	
 	@GetMapping("/usuario")
-	public List<String> getUsuarios() {
+	public List<Usuario> getUsuarios() {
 		System.out.println("IN");
 		return usuarios;
 	}
 	
-	@GetMapping("/usuario/{index}")
-	public String getUsuario(@PathVariable Integer index) {
+	@GetMapping("/usuario/{id}")
+	public Usuario getUsuario(@PathVariable Integer id) {
 		System.out.println("IN");
-		return usuarios.get(index);
+		return usuarios.get(usuarios.indexOf(new Usuario(id, "")));
 	}
 }
